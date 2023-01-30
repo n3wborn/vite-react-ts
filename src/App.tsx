8e0987072d2b37f024d4752a2daf502372669a26
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-01-30 17:28:14 +0100

    feat: set default index

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,24 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-    const [count, setCount] = useState(0)
-
     return (
         <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo"/>
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
+            <div id="nbColors"></div>
+
+            <div className="buttons group">
+                <button className="button" id="randFirst">Random First</button>
+                <button className="button" id="randAll">Random All</button>
+                <button className="button" id="randLast">Random Last</button>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
+
+            <div id="colors"></div>
+
+            <div id="gradient"></div>
+
+            <div className="buttons group">
+                <button className="button" id="toLeft">to Left</button>
+                <button className="button" id="toRight">to Right</button>
             </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
         </div>
     )
 }
