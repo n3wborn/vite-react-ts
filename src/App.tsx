10e8e4875d503883cb5934827e64870717c0f5cd
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-01-31 17:41:25 +0100

    feat(App): add 45,135,225,315deg buttons

--- src/App.tsx
+++ src/App.tsx
@@ -114,6 +114,10 @@ function App() {
 
             <div className="buttons group">
                 <button className="button" id="toLeft">to Left</button>
+                <button className="button" id="to45">45</button>
+                <button className="button" id="to135">135</button>
+                <button className="button" id="to225">225</button>
+                <button className="button" id="to315">315</button>
                 <button className="button" id="toRight">to Right</button>
             </div>
         </div>
