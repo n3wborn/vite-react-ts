92534e1c8a66967f87591b5b3ac2c4365fc83657
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-01 14:36:56 +0100

    refac: use React 18 createRoot() to render
    see: https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { createRoot } from 'react-dom/client';
 import {Provider} from "react-redux";
-import {render} from "react-dom";
 
 import App from './App'
 import './index.css'
 import store from "./store";
 
+// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
+const container = document.getElementById('root')!;
+const root = createRoot(container);
 const rootReactElement = (
     <React.StrictMode>
         <Provider store={store}>
@@ -14,6 +17,4 @@ const rootReactElement = (
     </React.StrictMode>
 )
 
-const target = document.getElementById('root')
-
-render(rootReactElement, target)
+root.render(rootReactElement);
