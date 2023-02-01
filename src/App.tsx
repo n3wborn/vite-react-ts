e3ee6c4f30be9e333f3f46658d27bbf61ec478f5
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-01 11:55:06 +0100

    refac: cleanup App

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,10 @@
 import './App.css'
-import store from "./store";
-import {randomizeFirstColor, randomizeLastColor} from "./actions";
 import {
     NbColors,
     Controls,
     Gradient,
     Direction,
 } from "./components"
-
-// store
-//  - Read      => getState() : get actual state
-//  - Write     => dispatch() : change state
-//  - Subscribe => subscribe() : subscribe to state update/modifications
-
-const renderNbColors = () => {
-    const state = store.getState()
-    const {nbColors} = state
-    const nbColorsEl = document.getElementById('nbColors')
-
-    nbColorsEl
-        ? nbColorsEl.innerHTML = `${nbColors} colors generated`
-        : null
-}
-
-store.subscribe(renderNbColors)
-
-// controls
-document.getElementById('randAll')
-    ?.addEventListener('click', () => {
-            store.dispatch(randomizeFirstColor())
-            store.dispatch(randomizeLastColor())
-        }
-    )
 
 export default function App() {
     return (
