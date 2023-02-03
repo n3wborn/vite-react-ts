0fba075dd066acb5097a769bb5b5c1d45db2a2e8
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-03 10:09:38 +0100

    feat: add Redux Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import {createRoot} from 'react-dom/client';
+import {Provider} from "react-redux";
 
 import App from './App'
 import './index.css'
+import store from "./store";
 
 // https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
 const container = document.getElementById('root')!;
 const root = createRoot(container);
+
 const rootReactElement = (
     <React.StrictMode>
+        <Provider store={store}>
             <App/>
+        </Provider>
     </React.StrictMode>
 )
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,6 @@ import {devToolsEnhancer} from "redux-devtools-extension";
 
 // deprecated way of doing things :/
 // https://redux.js.org/introduction/why-rtk-is-redux-today
-const store = createStore(convertReducer)
+const store = createStore(convertReducer, devToolsEnhancer())
 
 export default store
