866eabb4af18e8ad277d697ee8d67077c6d94c98
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-07 12:33:49 +0100

    deps: add @styled/typescript-styled-plugin

--- src/components/Chat/ChatStyled.ts
+++ src/components/Chat/ChatStyled.ts
@@ -3,8 +3,9 @@ import theme, {lighten} from "../../styles/theme"
 
 const ChatStyled = styled.div`
     width: 33vw;
-    background-color: ${lighten(theme.mainColor, .60)};
+    background-color: ${lighten(theme.mainColor, .20)};
     box-shadow: 0 0 3em rgba(0, 0, 0, .4);
+    border-radius: 5px;
 `
 
 export default ChatStyled
--- src/components/Messages/MessagesStyled.ts
+++ src/components/Messages/MessagesStyled.ts
@@ -8,12 +8,15 @@ const MessagesStyled = styled.div`
     align-items: center;
     overflow-x: hidden;
     overflow-y: scroll;
+    &::-webkit-scrollbar {
+        background-color: none;
+    }
     padding: 2rem;
     max-width: 700px;
     background: ${theme.bg};
     color: ${theme.mainColor};
     font-size: bold;
-    border-radius: 5px;
+    border-radius: 5px 5px 0px 0px;
 `
 
 export default MessagesStyled
