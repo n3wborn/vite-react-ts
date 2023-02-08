7d77fe5dafcec107100cecd2d982f094fc7de29b
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-08 17:46:14 +0100

    fix: React render

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {store} from "./store";
+import { createRoot } from 'react-dom/client';
 import {Provider} from "react-redux";
-import App from './App'
 import '@picocss/pico'
 
-ReactDOM.render(
+import {store} from "./store";
+import App from './App'
+
+// https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#updates-to-client-rendering-apis
+const container = document.getElementById('root');
+const root = createRoot(container!)
+
+root.render(
     <Provider store={store}>
         <App/>
     </Provider>,
-    document.getElementById('root')
-)
+);
