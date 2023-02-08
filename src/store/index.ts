25d76d12e13fc4fd44769fd273b6e15752555ba0
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-08 17:39:07 +0100

    feat: import todoSlice into our store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
+// this import represent our reducers from todoSlice
+import todoReducer from '../features/todo/todoSlice'
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        // add our reducers from features/todo/todoSlice
+        todo: todoReducer,
+    },
 })
 
 // from https://redux-toolkit.js.org/tutorials/quick-start
