196280a6afc5ea792141b7efd908221de2b2aca5
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-10 12:47:49 +0100

    feat(Tasklist): add useGetUserTodosByUserIdQuery results

--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -1,20 +1,48 @@
-import {useSelector} from "react-redux";
-import TaskItem from "./TaskItem";
-import {RootState} from "../store";
+import {useSelector} from 'react-redux'
+import {RootState} from '../store'
+import TaskItem from './TaskItem'
+import {useGetUserTodosByUserIdQuery} from '../services/todos'
 
 const TasksList = () => {
+    // local tasks
     const tasks = useSelector((state: RootState) => state.todo)
+    const {data, error, isLoading} = useGetUserTodosByUserIdQuery(5)
 
     return (
         <>
-            {tasks.map((t) => (
-                <TaskItem
-                    task={t}
-                    key={t.id}
-                />
-            ))}
-        </>
-    );
-};
+            {/*map over local tasks*/}
+            {
+                (
+                    tasks.map((t) => (
+                        <TaskItem
+                            task={t}
+                            key={t.id}
+                        />
+                    ))
 
-export default TasksList;
+                )
+            }
+
+            {/*map over distant tasks*/}
+            {error ? (
+                <>Oh no, there was an error while tryinf to fetch online tasks</>
+            ) : isLoading ? (
+
+                <>Loading...</>
+            ) : data ?
+                (
+                    <>
+                        {data.map((t) => (
+                            <TaskItem
+                                task={t}
+                                key={t.id}
+                            />
+                        ))}
+                    </>
+                ) : null
+            }
+        </>
+    )
+}
+
+export default TasksList
