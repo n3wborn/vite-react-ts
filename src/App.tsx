729da3f4bed20e6a804a7a4423f2e40235b35e7c
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-15 17:46:48 +0100

    feat(TaskHeader): Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import {TasksList} from "./components";
+import TasksHeader from "./components/TaskHeader";
 
 export default function App() {
 
     return (
         <div className="container">
+            <TasksHeader/>
             <article>
                 <TasksList/>
             </article>
--- src/components/TaskHeader.tsx
+++ src/components/TaskHeader.tsx
@@ -1,13 +1,34 @@
-const TasksHeader = ({tasks}) => {
-    const incompleteTasks = tasks.filter((t) => t.completed === false)
+import {useGetUserTodosQuery} from "../features/api/todos";
+import {Activity, Check} from "react-feather";
 
+export const PostsCountStat = () => {
+    const {data: tasks} = useGetUserTodosQuery(5)
+
+    if (!tasks) return null
+
+    const incompleteTasks = tasks.filter(t => !t.completed)
+
+    if (incompleteTasks === 0) {
+        return (
+            <p><Check color="green"/> Tasks done. Nice Job !</p>
+        )
+    }
+
+    return (
+        <>
+            <p>
+                <Activity color='#c6282899'/> Tasks to do: <strong>{incompleteTasks.length}</strong>
+            </p>
+        </>
+    )
+}
+
+const TasksHeader = () => {
     return (
         <>
             <header>
                 <h1>React - Redux Toolkit / Todo List</h1>
-                    <p>
-                        Tasks to do: <strong>{incompleteTasks.length}</strong>
-                    </p>
+                <PostsCountStat/>
             </header>
         </>
     )
