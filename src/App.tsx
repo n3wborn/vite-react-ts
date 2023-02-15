9adeecdb229b095d7d2c92c0e16bcded31f1b749
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-15 09:50:02 +0100

    feat: add Tasklist bask to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import {TasksList} from "./components";
+
 export default function App() {
 
     return (
         <div className="container">
             <article>
                 {/*<TasksHeader tasks={} key={}/>*/}
-                {/*<TasksList tasks={} key={}/>*/}
+                <TasksList />
                 <footer>
                     {/*<TaskForm/>*/}
                 </footer>
