39f664e62f083eeb173f9d297d5b22deb8fcbcb8
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-15 15:35:39 +0100

    feat(Tasklist): add polling & refetch

--- src/components/Taskslist.tsx
+++ src/components/Taskslist.tsx
@@ -3,7 +3,11 @@ import {TaskItem} from "./index";
 
 const TasksList = () => {
     // fetch data from (arbitrary) user with userId = 5
-    const { data = [], isLoading, isFetching, isError } = useGetUserTodosQuery(5)
+    const { data = [], isLoading, isFetching, isError } = useGetUserTodosQuery(5, {
+        pollingInterval: 10000,
+        refetchOnMountOrArgChange: true,
+        skip: false,
+    })
 
     if (isError) return <div>An error has occurred!</div>
     if (isLoading) return <div>isLoading ...</div>
