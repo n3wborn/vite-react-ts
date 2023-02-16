a1a25fe5d6f848d41e69f0833767174bb6771cc0
Stéphane Pluchart <spluchart@debutant-e.pro>
2023-02-16 16:03:04 +0100

    feat(api): add Queries and Mutations
    - toggleCompleted
    - updateTodo
    - deleteTodo
    - addPost

--- src/features/api/todos.ts
+++ src/features/api/todos.ts
@@ -1,5 +1,5 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import {Task} from "../../components/TaskItem";
+import {Task} from "../../components/TaskItem"
 
 type TasksResponse = Task[]
 
@@ -16,13 +16,65 @@ export const api = createApi({
             providesTags: (result) =>
                 result
                     ? [
-                        ...result.map(({ id }) => ({ type: 'Task' as const, id })),
-                        { type: 'Task', id: 'LIST' },
+                        ...result.map(({id}) => ({type: 'Task' as const, id})),
+                        {type: 'Task', id: 'LIST'},
                     ]
-                    : [{ type: 'Task', id: 'LIST' }],
+                    : [{type: 'Task', id: 'LIST'}],
+        }),
+        toggleCompleted: builder.mutation({
+            /// user 5's task title "suscipit qui totam" =>  todos?userId=5&title=suscipit%20qui%20totam
+            // list completed todos from userId 5 => '/todos?userId=5&completed=true'
+            // patch "completed" todos with id=1 => PATCH '/todos/${id}'
+            query: ({id, completed}) => ({
+                url: `/todos/${id}`,
+                method: 'PATCH',
+                completed: completed,
+            }),
+            transformResponse: (response: { data: Task }, meta, arg) => response.data,
+            transformErrorResponse: (
+                response: { status: string | number },
+                meta,
+                arg
+            ) => response.status,
+            invalidatesTags: ['Task'],
+        }),
+        updateTodo: builder.mutation({
+            // examples of api url/query:
+            // - user 5's task title "suscipit qui totam" =>  todos?userId=5&title=suscipit%20qui%20totam
+            // - completed todos from userId 5 => '/todos?userId=5&completed=true'
+            query: ({id, ...patch}) => ({
+                url: `/todos/${id}`,
+                method: 'PUT',
+                body: patch,
+            }),
+            invalidatesTags: (result, error, id) => [{type: 'Task', id}],
+        }),
+        deleteTodo: builder.mutation<{ success: boolean; id: number }, number>({
+            query(id) {
+                return {
+                    url: `/todos/${id}`,
+                    method: 'DELETE',
+                }
+            },
+            // Invalidates all queries that subscribe to this Post `id` only.
+            invalidatesTags: (result, error, id) => [{type: 'Task', id}],
+        }),
+        addPost: builder.mutation({
+            query: (body) => ({
+                url: `/posts`,
+                method: 'POST',
+                body,
+            }),
+            invalidatesTags: [{type: 'Task', id: 'LIST'}],
         }),
     }),
 })
 
 // Export hooks for usage in functional components
-export const {useGetUserTodosQuery} = api
+export const {
+    useGetUserTodosQuery,
+    useToggleCompletedMutation,
+    useDeleteTodoMutation,
+    useAddPostMutation,
+    useUpdateTodoMutation,
+} = api
